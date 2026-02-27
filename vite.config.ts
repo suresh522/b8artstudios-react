b78771df929f0790c2d1bbb8887d456ebb5bfdd0
suresh522 <kajasuresh522@gmail.com>
2026-02-27 16:18:59 +0530

    route pathh set completed

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/b8artstudios-react",
+  base: "/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
